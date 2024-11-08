9f3a807fabc9d3ed4c084b3de7ea8c2a906329a0
muiddd <muidabdulmuid99@gmail.com>
2024-11-08 10:25:06 +0700

    update lek

--- Tugas01.java
+++ Tugas01.java
@@ -8,18 +8,20 @@ public class Tugas01 {
         String namaResponden = " ";
         int i = 0;
         int pilJawaban = 0;
+        double rata=0;
 
         for (i = 0; i < responden.length; i++) {
             System.out.print("Masukkan nama responden ke-" + (i + 1) + ": ");
             namaResponden = input.nextLine();
+            int totalJawaban =0;
             for (int j = 0; j < 6; j++) {
-                System.out.print("Masukkan jawaban (1-5) untuk soal ke-" + (j + 1) + ": ");
+                System.out.print("Masukkan jawaban (1-5) untuk pertanyaan ke-" + (j + 1) + ": ");
                 pilJawaban = input.nextInt();
                 input.nextLine();
+                totalJawaban+=pilJawaban;
             }
+            rata = (double) totalJawaban/6;
+            System.out.println("Rata rata survei per responden: " +rata);   
         }
-        System.out.println("------HASIL SURVEI------");
-        System.out.println("Responden ke-" + (i + 1) + " dengan nama: " + namaResponden);
-        System.out.println("Jawaban soal ke-" + (i + 1) + ": " + pilJawaban);
     }
 }
