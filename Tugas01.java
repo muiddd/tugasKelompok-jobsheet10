0c875ef6905715d39f313867d7b54f42501b2fc1
qulbyazz <qulbyazzumy@gmail.com>
2024-11-08 09:53:31 +0700

    update 1

--- Tugas01.java
+++ Tugas01.java
@@ -5,16 +5,21 @@ public class Tugas01 {
         Scanner input = new Scanner(System.in);
         String[][] responden = new String[10][6];
 
-        String namaResponden;
+        String namaResponden = " ";
+        int i = 0;
+        int pilJawaban = 0;
 
-        for (int i = 0; i < responden.length; i++) {
+        for (i = 0; i < responden.length; i++) {
             System.out.print("Masukkan nama responden ke-" + (i + 1) + ": ");
             namaResponden = input.nextLine();
             for (int j = 0; j < 6; j++) {
                 System.out.print("Masukkan jawaban (1-5) untuk soal ke-" + (j + 1) + ": ");
-                int pilJawaban = input.nextInt();
+                pilJawaban = input.nextInt();
                 input.nextLine();
             }
         }
+        System.out.println("------HASIL SURVEI------");
+        System.out.println("Responden ke-" + (i + 1) + " dengan nama: " + namaResponden);
+        System.out.println("Jawaban soal ke-" + (i + 1) + ": " + pilJawaban);
     }
 }
