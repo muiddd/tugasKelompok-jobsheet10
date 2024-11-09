24f093d21aedff931688b2abea2b1dc3c141dd9c
qulbyazz <qulbyazzumy@gmail.com>
2024-11-09 09:32:36 +0700

    new update

--- Tugas01.java
+++ Tugas01.java
@@ -5,23 +5,31 @@ public class Tugas01 {
         Scanner input = new Scanner(System.in);
         String[][] responden = new String[10][6];
 
-        String namaResponden = " ";
-        int i = 0;
-        int pilJawaban = 0;
-        double rata=0;
+        String namaResponden;
+        int i = 0, pilJawaban = 0;
+        double rata = 0, rataKeseluruhan, rataPertanyaan = 0;
+        int[] totalPertanyaan = new int[6];
 
         for (i = 0; i < responden.length; i++) {
             System.out.print("Masukkan nama responden ke-" + (i + 1) + ": ");
             namaResponden = input.nextLine();
-            int totalJawaban =0;
+            int totalJawaban = 0;
             for (int j = 0; j < 6; j++) {
                 System.out.print("Masukkan jawaban (1-5) untuk pertanyaan ke-" + (j + 1) + ": ");
                 pilJawaban = input.nextInt();
                 input.nextLine();
-                totalJawaban+=pilJawaban;
+                totalJawaban += pilJawaban;
+                totalPertanyaan[j] += pilJawaban;
             }
-            rata = (double) totalJawaban/6;
-            System.out.println("Rata rata survei per responden: " +rata);   
+            rata = (double) totalJawaban / 6;
+            System.out.println("Rata rata survei per responden: " + rata);
         }
+        System.out.println("\n Rata rata per pertanyaan");
+        for (int j = 0; j < 6; j++) {
+            rataPertanyaan = totalPertanyaan[j]/responden.length;
+            System.out.println("Rata rata pertanyaan ke-"+(j+1)+": " +rataPertanyaan);
+        }
+        rataKeseluruhan = rata + rataPertanyaan;
+        System.out.println("Rata rata keseluruhan: "+rataKeseluruhan);
     }
 }
